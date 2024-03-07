80090ed11d48afd1bc3a3ce27d8d7e31493ef445
MildyS <ilovsky.ilovsky@gmail.com>
2024-03-07 22:57:39 +0100

    Az teraz pridanie MyImages

---
+++ data/02_images.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE
+    "MyImages" (
+      "Id" bigserial PRIMARY KEY NOT NULL,
+      "Type" varchar(20),
+      "Title" text,
+      "Content" text,
+      "Source" text,
+      "Decrypted" varchar(20),
+      "CreationDate" timestamp DEFAULT CURRENT_TIMESTAMP,
+      "UserId" bigint REFERENCES "MyUsers"("Id")
+);
