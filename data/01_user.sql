f1b48502c2daeedb7d8bcc5427d31da30049860d
MildyS <ilovsky.ilovsky@gmail.com>
2024-03-07 22:57:00 +0100

    Pridanie poli do MyUsers a vytvorenie MyImages

--- data/01_user.sql
+++ data/01_user.sql
@@ -2,6 +2,8 @@ CREATE TABLE
   "MyUsers" (
     "Id" bigserial PRIMARY KEY NOT NULL,
     "Email" text NOT NULL UNIQUE,
+    "FirstName" text,
+    "LastName" text,
     "Password" text NOT NULL,
     "Otp" text NOT NULL,
     "IsVerified" boolean NOT NULL,
