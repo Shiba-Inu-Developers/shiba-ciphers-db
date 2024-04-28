01cb312ed3a7ed5a211fb1d70202a0ef519bfc43
MildyS <ilovsky.ilovsky@gmail.com>
2024-04-28 23:27:04 +0200

    predbežná zmena DB

--- data/02_images.sql
+++ data/02_images.sql
@@ -5,6 +5,9 @@
       "Title" text,
       "Content" text,
       "Source" text,
+      "Extension" varchar(10),
+      "Segmented" text,
+      "Encrypted" text,
       "Decrypted" varchar(20),
       "CreationDate" timestamp DEFAULT CURRENT_TIMESTAMP,
       "UserId" bigint REFERENCES "MyUsers"("Id")
