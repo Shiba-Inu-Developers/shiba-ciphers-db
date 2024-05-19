7d4275531d19002a8e8a62e8749a0cf5e054ff76
xmikula <62661841+xmikula@users.noreply.github.com>
2024-05-19 17:16:41 +0200

    Add cascade delete

--- data/03_segments.sql
+++ data/03_segments.sql
@@ -1,6 +1,9 @@
 CREATE TABLE segments (
-    image_id bigserial references images(id) not null,
+    image_id bigserial not null,
     data text not null,
     content text,
-    primary key(image_id, data)
+    primary key(image_id, data),
+    foreign key(image_id) references images(id) on
+    delete
+        cascade
 );
